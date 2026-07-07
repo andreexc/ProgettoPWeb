f623b6802a710ceec19c20b9f86b72d409242380
Andrea <depaandrea05@gmail.com>
2026-07-07 16:25:34 +0200

    Password length fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
 -- Inserimento Utenti Obbligatori
 INSERT INTO users (username, password, enabled) VALUES
-    ('admin#team_20', '{noop}adm_id_20', true),
-    ('basic#team_20', '{noop}bsc_id_20', true),
-    ('pro#team_20', '{noop}pro_id_20', true),
-    ('prova#1#team_20', '{noop}prv_id_20', true);
+    ('admin#team_20', '{noop}ad_id_20', true),
+    ('basic#team_20', '{noop}bs_id_20', true),
+    ('pro#team_20', '{noop}pv_id_20', true),
+    ('prova#1#team_20', '{noop}pr_id_20', true);
 
 -- Assegnazione Ruoli
 INSERT INTO authorities (username, authority) VALUES
@@ -14,3 +14,4 @@ INSERT INTO authorities (username, authority) VALUES
 
 
 -- @TODO criptare le password con BCrypt
+-- attenzione {noop} password
